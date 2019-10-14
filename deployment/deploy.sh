748ea34b54b6f183f36bbd21563a5d9914ca670b
Jerome Moore <romeski@Jeromes-MacBook-Pro.local>
2019-10-14 11:22:47 -0400

    update to deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -8,5 +8,5 @@ aws ecs register-task-definition --cli-input-json file://TASKDEF.json > REGISTER
 TASKDEFINITION_ARN=$( < REGISTERED_TASKDEF.json jq .taskDefinition.taskDefinitionArn )
 
 # create or update service
-sed "s,@@TASKDEFINITION_ARN@@,$TASKDEFINITION_ARN," <service-$0-nginx.json >SERVICEDEF.json
-aws ecs $0-service --cli-input-json file://SERVICEDEF.json | tee SERVICE.json
+sed "s,@@TASKDEFINITION_ARN@@,$TASKDEFINITION_ARN," <service-$1-nginx.json >SERVICEDEF.json
+aws ecs $1-service --cli-input-json file://SERVICEDEF.json | tee SERVICE.json
